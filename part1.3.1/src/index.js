3f82bf37f2167ab5d67ff944537ef93851a771d0
Brian Henrichs <henbrc@gmail.com>
2021-01-28 10:46:48 -0800

    excercise 2.5 done, dunno about node_modules

--- part1.3.1/src/index.js
+++ part1.3.1/src/index.js
@@ -25,7 +25,7 @@ const Content = ({ course }) => {
   
     console.log("the course is",course)
     return(
-      <ul>
+      <ul >
     {course.parts.map(parts => 
       <li key={parts.id}>
         {parts.name} {parts.exercises}
@@ -46,32 +46,68 @@ const Course =({course})=> {
   )
 }
 
-
-
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
+const Courses =(props)=>{
+  console.log("courses id are",props)
+  return(
+    <ul>
+     {props.courses.map( course =>
+     <li key={course.id}>
+       <Course course={course}/>
+       </li>
+       )}
+     </ul>
+  )
+ 
   }
 
-  return <Course course={course} />
+  const App = () => {
+    const courses = [
+      {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
+      {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
+  
+
+  return <Courses courses={courses} />
 }
 
 
